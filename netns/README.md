5924bf7b01628c823a62a1049f377e28f8cd1ea4
David Timber <dxdt@dev.snart.me>
2024-09-06 23:05:17 +0200

    Add IPv4 support ...
    - the install target overrides the existing config
    - fix some grammatical errors in doc
    - change env file format - breaks existing config
    - Busybox sysctl applet support
    - add "rmns_q" command for ExecStartPre clean up

--- netns/README.md
+++ netns/README.md
@@ -13,7 +13,7 @@ to internet           |  MTU 1280 segment   |               to httpd
 ```
 
 <pre>
-$ tracepath fd12:34::3:1500:0
+$ tracepath -n fd12:34::3:1500:0
  1?: [LOCALHOST]                        0.025ms pmtu 1500
  1:  fd12:34::1:1280:0                                     0.128ms
  1:  fd12:34::1:1280:0                                     0.156ms
@@ -21,15 +21,22 @@ $ tracepath fd12:34::3:1500:0
  2:  fd12:34::2:1280:0                                     0.235ms
  3:  fd12:34::3:1500:0                                     0.188ms reached
      Resume: pmtu 1280 hops 3 back 3
+$ tracepath -n 10.12.80.34
+ 1?: [LOCALHOST]                      pmtu 1500
+ 1:  10.12.80.2                                            0.138ms
+ 1:  10.12.80.2                                            0.042ms
+ 2:  10.12.80.2                                            0.045ms <b>pmtu 1280</b>
+ 2:  10.12.80.18                                           0.055ms
+ 3:  10.12.80.34                                           0.051ms reached
+     Resume: pmtu 1280 hops 3 back 3
 </pre>
 
-3 netns are utilised so that the MTU 1280 links are hidden from both the
-internet facing gateway and httpd. This forces httpd to announce its MSS as 1440
-octets and PMTUD is carried out for each endpoint from the internet. The PMTU
-caching in **mtu1280-c** is disabled so that the test clients can get the same
-result all the time. The makefile recipe installs a "drop-in" service unit
-config to override `NetworkNamespacePath` so httpd is launched in the mtu1280-c
-netns.
+3 netns are create to hide the MTU 1280 links from both internet facing gateway
+and httpd. This forces httpd to announce its MSS as 1440 octets and PMTUD is
+carried out for each endpoint from the internet. The PMTU caching in
+**mtu1280-c** is disabled so that the test clients can get the same result at
+all times. The makefile recipe installs a "drop-in" service unit config to
+override `NetworkNamespacePath` so httpd is run in the mtu1280-c netns.
 
 The set up should perfectly simulate the kernel's behaviour. A few down sides
 over the original mtu1280d approach are
@@ -39,6 +46,13 @@ over the original mtu1280d approach are
 1. somewhat difficult to integrate netns to most programs as they're not
    designed with namespace in mind
 1. complexity making it difficult to troubleshoot
+
+## IPv4
+The script was originally written to test the PMTUD of IPv6, but the IPv4
+support has been added to test the PMTUD in IPv4 networks as well. As most
+operating system does not do IPv4 PMTUD by default, the use of special system
+API is required to set the DF flag. It's useful when any middlebox of the ISP
+blocks `Fragmentation Needed` or fragmented packets in general.
 
 ## Network Set Up
 See [/um/README.md#network-set-up](/um/README.md#network-set-up).
@@ -51,12 +65,12 @@ cp mtu1280-netns.env.sample mtu1280-netns.env
 vi mtu1280-netns.env
 ```
 
-Set `MTU1280NS_ADDR_A` to the address of the first router and so on.
+Set `MTU1280NS_ADDR6_*` to the addresses of the routers. For IPv4, set
+`MTU1280NS_ADDR4_*` as well. If you plan to use private IPv4 addresses, remember
+to turn masquerade on and set port forwarding.
 
-- Use `MTU1280NS_ADDR_GW` if you want the script to set the IPv6 address of the
-  host (it is best if it's left with NetworkManager, though)
 - Set `MTU1280NS_NO_OFFLOAD` to "true" to disable offloading features that
-  interfere with the packet capture result (for debugging)
+  can interfere with the packet capture result when debugging
 
 ```sh
 # Stop http as the recipe will override some settings in httpd.service
@@ -85,13 +99,13 @@ sudo ip -6 -n mtu1280-c route
 # The listening ports will show up in the ns, not the default so the ports won't
 #  show up in the ss command run without the ip or nsenter command
 sudo ip netns exec mtu1280-c ss -tlnp
-
-# The PMTU cache table entries will pile up in production
-sudo ip -6 -n mtu1280-c route show cache
 ```
 
 ## Other Useful Info
 All settings are netns-local.
 
-- `ip -6 route flush cache` flushes the PMTU cache
-- `sysctl -w net.ipv6.route.mtu_expires=0` disables PMTU caching
+- Use `ip -4 route show cache` and `ip -6 route show cache` to show PMTU cache
+- Use `ip -6 route flush cache` and `ip -4 route flush cache` to flush the PMTU
+  cache
+- `sysctl -w net.ipv6.route.mtu_expires=0` and `sysctl -w
+  net.ipv4.route.mtu_expires=0` disables PMTU caching
--- netns/mtu1280-netns.sh
+++ netns/mtu1280-netns.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 [ -z "$MTU1280NS_NO_OFFLOAD" ] && MTU1280NS_NO_OFFLOAD=false
 
+safe_run () {
+	if [ -z "$1" ]; then
+		return
+	fi
+
+	shift
+	$@
+}
+
 mkns () {
 	set -e
 	local ll
@@ -8,11 +17,11 @@ mkns () {
 	do_make_ns () {
 		ip netns add "$1"
 		ip netns exec "$1" sysctl -qw \
-			"net.ipv4.conf.all.forwarding = 1" \
-			"net.ipv4.conf.default.forwarding = 1" \
-			"net.ipv4.ip_forward = 1" \
-			"net.ipv6.conf.default.forwarding = 1" \
-			"net.ipv6.conf.all.forwarding = 1"
+			"net.ipv4.conf.all.forwarding=1" \
+			"net.ipv4.conf.default.forwarding=1" \
+			"net.ipv4.ip_forward=1" \
+			"net.ipv6.conf.default.forwarding=1" \
+			"net.ipv6.conf.all.forwarding=1"
 	}
 
 	get_ll_addr () {
@@ -35,6 +44,10 @@ mkns () {
 		done
 	}
 
+	filter_cidr () {
+		echo "${1%/*}"
+	}
+
 	no_offload () {
 		if ! "$MTU1280NS_NO_OFFLOAD"; then
 			return
@@ -52,7 +65,8 @@ mkns () {
 	do_make_ns mtu1280-c
 
 	# disable PMTU caching in mtu1280-c
-	ip netns exec mtu1280-c sysctl -qw "net.ipv6.route.mtu_expires = 0"
+	ip netns exec mtu1280-c sysctl -qw "net.ipv4.route.mtu_expires=0"
+	ip netns exec mtu1280-c sysctl -qw "net.ipv6.route.mtu_expires=0"
 
 	# link up system default ns -> mtu1280-a
 	ip link add veth-mtu1280-a type veth peer name veth-mtu1280-gw netns mtu1280-a
@@ -67,12 +81,19 @@ mkns () {
 	no_offload veth-mtu1280-c mtu1280-b
 	no_offload veth-mtu1280-b mtu1280-c
 
-	if [ ! -z "$MTU1280NS_ADDR_GW" ]; then
-		ip -6 addr add $MTU1280NS_ADDR_GW dev veth-mtu1280-a
-	fi
-	ip -n mtu1280-a -6 addr add $MTU1280NS_ADDR_A dev veth-mtu1280-gw
-	ip -n mtu1280-b -6 addr add $MTU1280NS_ADDR_B dev veth-mtu1280-a
-	ip -n mtu1280-c -6 addr add $MTU1280NS_ADDR_C dev veth-mtu1280-b
+	safe_run "$MTU1280NS_ADDR4_A0" ip -4 addr add "$MTU1280NS_ADDR4_A0" dev veth-mtu1280-a
+
+	safe_run "$MTU1280NS_ADDR4_A1" ip -n mtu1280-a -4 addr add "$MTU1280NS_ADDR4_A1" dev veth-mtu1280-gw
+	safe_run "$MTU1280NS_ADDR4_B0" ip -n mtu1280-a -4 addr add "$MTU1280NS_ADDR4_B0" dev veth-mtu1280-b
+
+	safe_run "$MTU1280NS_ADDR4_B1" ip -n mtu1280-b -4 addr add "$MTU1280NS_ADDR4_B1" dev veth-mtu1280-a
+	safe_run "$MTU1280NS_ADDR4_C0" ip -n mtu1280-b -4 addr add "$MTU1280NS_ADDR4_C0" dev veth-mtu1280-c
+
+	safe_run "$MTU1280NS_ADDR4_C1" ip -n mtu1280-c -4 addr add "$MTU1280NS_ADDR4_C1" dev veth-mtu1280-b
+
+	safe_run "$MTU1280NS_ADDR6_A" ip -n mtu1280-a -6 addr add "$MTU1280NS_ADDR6_A" dev veth-mtu1280-gw
+	safe_run "$MTU1280NS_ADDR6_B" ip -n mtu1280-b -6 addr add "$MTU1280NS_ADDR6_B" dev veth-mtu1280-a
+	safe_run "$MTU1280NS_ADDR6_C" ip -n mtu1280-c -6 addr add "$MTU1280NS_ADDR6_C" dev veth-mtu1280-b
 
 	ip link set veth-mtu1280-a up
 	ip -n mtu1280-a link set lo up
@@ -87,24 +108,33 @@ mkns () {
 	ip -n mtu1280-c link set veth-mtu1280-b up
 
 	# default route from mtu1280-a to system
+	ip -n mtu1280-a -4 route add default dev veth-mtu1280-gw via $(filter_cidr "$MTU1280NS_ADDR4_A0")
 	ip -n mtu1280-a -6 route add default dev veth-mtu1280-gw via $(get_ll_addr veth-mtu1280-a)
 	# default route from mtu1280-b to mtu1280-a (mtu 1280 segment)
+	ip -n mtu1280-b -4 route add default dev veth-mtu1280-a via $(filter_cidr "$MTU1280NS_ADDR4_B0") mtu 1280
 	ip -n mtu1280-b -6 route add default dev veth-mtu1280-a via $(get_ll_addr veth-mtu1280-b mtu1280-a) mtu 1280
 	# default route from mtu1280-c to mtu1280-b
+	ip -n mtu1280-c -4 route add default dev veth-mtu1280-b via $(filter_cidr "$MTU1280NS_ADDR4_C0")
 	ip -n mtu1280-c -6 route add default dev veth-mtu1280-b via $(get_ll_addr veth-mtu1280-c mtu1280-b)
 
 	# static route from system default ns to inner
 	ll=$(get_ll_addr veth-mtu1280-gw mtu1280-a)
-	ip -6 route add $MTU1280NS_ADDR_A dev veth-mtu1280-a via $ll
-	ip -6 route add $MTU1280NS_ADDR_B dev veth-mtu1280-a via $ll
-	ip -6 route add $MTU1280NS_ADDR_C dev veth-mtu1280-a via $ll
+	safe_run "$MTU1280NS_ADDR4_B0" ip -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_B0")" dev veth-mtu1280-a via "$(filter_cidr "$MTU1280NS_ADDR4_A1")"
+	safe_run "$MTU1280NS_ADDR4_B1" ip -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_B1")" dev veth-mtu1280-a via "$(filter_cidr "$MTU1280NS_ADDR4_A1")"
+	safe_run "$MTU1280NS_ADDR4_C0" ip -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_C0")" dev veth-mtu1280-a via "$(filter_cidr "$MTU1280NS_ADDR4_A1")"
+	safe_run "$MTU1280NS_ADDR4_C1" ip -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_C1")" dev veth-mtu1280-a via "$(filter_cidr "$MTU1280NS_ADDR4_A1")"
+	safe_run "$MTU1280NS_ADDR6_A" ip -6 route add "$MTU1280NS_ADDR6_A" dev veth-mtu1280-a via $ll
+	safe_run "$MTU1280NS_ADDR6_B" ip -6 route add "$MTU1280NS_ADDR6_B" dev veth-mtu1280-a via $ll
+	safe_run "$MTU1280NS_ADDR6_C" ip -6 route add "$MTU1280NS_ADDR6_C" dev veth-mtu1280-a via $ll
 	# static route from mtu1280-a to inner (mtu 1280 segment)
 	ll=$(get_ll_addr veth-mtu1280-a mtu1280-b)
-	ip -n mtu1280-a -6 route add $MTU1280NS_ADDR_B dev veth-mtu1280-b via $ll mtu 1280
-	ip -n mtu1280-a -6 route add $MTU1280NS_ADDR_C dev veth-mtu1280-b via $ll mtu 1280
+	safe_run "$MTU1280NS_ADDR4_C0" ip -n mtu1280-a -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_C0")" dev veth-mtu1280-b via "$(filter_cidr "$MTU1280NS_ADDR4_B1")" mtu 1280
+	safe_run "$MTU1280NS_ADDR4_C1" ip -n mtu1280-a -4 route add "$(filter_cidr "$MTU1280NS_ADDR4_C1")" dev veth-mtu1280-b via "$(filter_cidr "$MTU1280NS_ADDR4_B1")" mtu 1280
+	safe_run "$MTU1280NS_ADDR6_B" ip -n mtu1280-a -6 route add "$MTU1280NS_ADDR6_B" dev veth-mtu1280-b via $ll mtu 1280
+	safe_run "$MTU1280NS_ADDR6_C" ip -n mtu1280-a -6 route add "$MTU1280NS_ADDR6_C" dev veth-mtu1280-b via $ll mtu 1280
 	# static route from mtu1280-b to inner
 	ll=$(get_ll_addr veth-mtu1280-b mtu1280-c)
-	ip -n mtu1280-b -6 route add $MTU1280NS_ADDR_C dev veth-mtu1280-c via $ll
+	safe_run "$MTU1280NS_ADDR6_C" ip -n mtu1280-b -6 route add "$MTU1280NS_ADDR6_C" dev veth-mtu1280-c via $ll
 }
 
 rmns () {
@@ -115,6 +145,10 @@ rmns () {
 	ip netns del mtu1280-a
 	ip netns del mtu1280-b
 	ip netns del mtu1280-c
+}
+
+rmns_q () {
+	rmns $@ 2> /dev/null > /dev/null
 }
 
 daemon () {
